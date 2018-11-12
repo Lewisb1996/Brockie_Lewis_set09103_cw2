5d4516cef5f13f0a120b59b644a721f93aa5e1eb
Lewisb1996 <lewisb76@icloud.com>
2018-11-12 11:06:27 +0000

    New files created

--- home.py
+++ home.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def root():
     return render_template('base.html')
 
+@app.route('/comingsoon')
+def comingsoon():
+    return render_template('comingsoon.html')
+
 if __name__ == ("__main__"):
     app.run(host='0.0.0.0', debug=True)
 
